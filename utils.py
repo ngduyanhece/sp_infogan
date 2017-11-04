506d0dd85a9d88ba4e0812e9583d9951bbb400f1
Donald Trump <dapxichlo@Donalds-MacBook-Pro.local>
2017-11-04 21:59:43 +0700

    finish model

---
+++ utils.py
@@ -0,0 +1,40 @@
+import os
+
+
+def remove_files(files):
+    """
+    Remove files from disk
+
+    args: files (str or list) remove all files in 'files'
+    """
+
+    if isinstance(files, (list, tuple)):
+        for f in files:
+            if os.path.isfile(os.path.expanduser(f)):
+                os.remove(f)
+    elif isinstance(files, str):
+        if os.path.isfile(os.path.expanduser(files)):
+            os.remove(files)
+
+
+def create_dir(dirs):
+    """
+    Create directory
+
+    args: dirs (str or list) create all dirs in 'dirs'
+    """
+
+    if isinstance(dirs, (list, tuple)):
+        for d in dirs:
+            if not os.path.exists(os.path.expanduser(d)):
+                os.makedirs(d)
+    elif isinstance(dirs, str):
+        if not os.path.exists(os.path.expanduser(dirs)):
+            os.makedirs(dirs)
+
+
+def setup_logging():
+    model_dir = "./saved_model"
+    fig_dir = "./images"
+    # Create if it does not exist
+    create_dir([fig_dir,model_dir])
